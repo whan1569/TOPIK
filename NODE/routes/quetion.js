3b4b0b78f6e8631990e8e6dbd6492bfd1b9667a4
Lee <whan4404@naver.com>
2022-03-19 21:38:59 +0900

    multer추가, 문제입력폼 추가

---
+++ NODE/routes/quetion.js
@@ -0,0 +1,30 @@
+var express = require('express');
+const dbcon = require("../config/db_con");
+var multer  = require('multer');
+var router = express.Router();
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/question');
+  },
+  filename: function (req, file, cb) {
+    cb(null, req.body.code+'.png');
+  }
+});
+var upload = multer({ storage: storage });
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('quetion', { title: 'quetion' });
+});
+router.get('/quetion_insert', function(req, res, next) {
+  res.render('quetion_insert', { title: 'quetion_insert' });
+});
+
+router.post('/quetion_insert', upload.single('file'),async function(req, res, next) {
+  const {code,num1,num2,num3,num4,answ} = req.body;
+  await dbcon("INSERT INTO question(code,num1,num2,num3,num4,answ) VALUES (?,?,?,?,?,?);", [code,num1,num2,num3,num4,answ]);
+  res.redirect("/");
+});
+
+module.exports = router;
--- NODE/routes/user.js
+++ NODE/routes/user.js
@@ -5,12 +5,12 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/',async  function(req, res, next) {
   var user = await dbcon("SELECT id FROM user");
-  res.render('users', { title: 'user', user: user });
+  res.render('user', { title: 'user', user: user });
 });
-router.get('/users_insert', function(req, res, next) {
-  res.render('users_insert', { title: 'users_insert'});
+router.get('/user_insert', function(req, res, next) {
+  res.render('user_insert', { title: 'users_insert'});
 });
-router.post('/users_insert', async function(req, res, next) {
+router.post('/user_insert', async function(req, res, next) {
   const { id,pw } = req.body;
   await dbcon("INSERT INTO user(id,pw) VALUES (?,?);", [id,pw]);
   res.redirect("/");
