984902a6ea93de8b5e02fa81541468586fc7b6ce
Lee <whan4404@naver.com>
2022-03-25 03:25:32 +0900

    회원가입 중복 처리

--- NODE/routes/user.js
+++ NODE/routes/user.js
@@ -12,7 +12,12 @@ router.get('/user_insert', function(req, res, next) {
 });
 router.post('/user_insert', async function(req, res, next) {
   const { id,pw } = req.body;
-  await dbcon("INSERT INTO user(id,pw) VALUES (?,?);", [id,pw]);
+  
+  const result = await dbcon("SELECT COUNT(*) as count FROM user WHERE id = ?", [id]);
+  const [{ count }] = result;
+  console.log(count);
+  if(count==0)
+    await dbcon("INSERT INTO user(id,pw) VALUES (?,?);", [id,pw]);
   res.redirect("/");
 });
 module.exports = router;
