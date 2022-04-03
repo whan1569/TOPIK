4e24ac02b2ba787ad2f646055ed88646cb6be06e
Lee <whan4404@naver.com>
2022-04-03 16:01:49 +0900

    체점 구현

--- NODE/routes/index.js
+++ NODE/routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const session = require('express-session');
 const dbcon = require("../config/db_con");
 var router = express.Router();
 
@@ -8,16 +9,36 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', user_id : req.session.user_id });
 });
 router.get('/logout', function(req, res, next) {
-  delete req.session.user_id;
+  req.session.destroy(function(){ req.session;});  
   res.redirect('/');
 });
 
 router.get('/test',async  function(req, res, next) {
-  num = req.query.num;
-  num++;
+  var num = req.query.num++;
   var a=206000+num;
-  console.log(a);
-  var quetion = await dbcon("SELECT * FROM quetion WHERE code = ?",[a]);
-  res.render('test', {quetion:quetion,num:num});
+  var [quetion] = await dbcon("SELECT * FROM quetion WHERE code = ?",[a]);
+  req.session.answ = quetion.answ;
+  res.render('test', {answ : quetion.answ,num1 : quetion.num1, num2 : quetion.num2, num3 : quetion.num3 , num4 : quetion.num4 ,code : quetion.code , num:num});
+});
+router.post('/test',async  function(req, res, next) {
+  var num = req.query.num++;
+  if(num<50)
+  {
+    if(!req.session.test)
+     req.session.test = [];
+     console.log(req.session.answ);
+     console.log(req.body.ans);
+    if(req.body.ans==req.session.answ)
+      req.session.test[num]=0;
+    else
+      req.session.test[num]=1;
+    console.log(req.session);
+    num++;
+    res.redirect('test?num='+num);
+  }
+  else
+  {
+    res.redirect('/');
+  }
 });
 module.exports = router;
